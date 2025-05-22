54b7d2fc42b27286e966d8b196f546afcba1d21a
Ben West <Xodarap@users.noreply.github.com>
2025-05-22 13:26:41 -0700

    harness

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
-from inspect_ai import eval
+from inspect_ai import eval_set
 from eval import bounty
+from datetime import datetime
+import os
+import shutil
 load_dotenv()
 
 MODELS = [
@@ -8,7 +11,18 @@ MODELS = [
     "openai/gpt-4o",
     # "google/gemini-1.5-pro"
 ]
+# Create a folder for logs if it doesn't exist
+log_dir = "logs-temp"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+eval_set(bounty(), model=[
+    "anthropic/claude-3-5-sonnet-latest",
+    "openai/gpt-4o",
+    # "google/gemini-1.5-pro"
+], log_dir=log_dir)
 
-for model in MODELS:
-    print(f"Running model: {model}")
-    eval(bounty(), model=model)
+
+now = datetime.now().strftime("%Y%m%d_%H%M%S")
+destination_dir = f"logs/{now}"
+os.makedirs(os.path.dirname(destination_dir), exist_ok=True)
+shutil.move(log_dir, destination_dir)
